8cbe3b07aed58c816f762fc0d0c68a358d871cc3
Asmaa Maakel <asmaamaakel@outlook.com>
2018-09-09 04:06:27 +0100

    assignment done!

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -1,7 +1,3 @@
-// *******************************
-// START HERE IF YOU WANT A MORE CHALLENGING STARTING POINT FOR THIS ASSIGNMENT
-// *******************************
-//
 // Module 4 Assignment Instructions.
 //
 // The idea of this assignment is to take an existing array of names
@@ -9,14 +5,22 @@
 // The program should say "Hello" to any name except names that start with a "J"
 // or "j", otherwise, the program should say "Good Bye". So, the final output
 // on the console should look like this:
-/*
-Hello Yaakov
-Good Bye John
-Good Bye Jen
-Good Bye Jason
-Hello Paul
-Hello Frank
-Hello Larry
-Hello Paula
-Hello Laura
-Good Bye Jim
+
+// Hello Yaakov
+// Good Bye John
+// Good Bye Jen
+// Good Bye Jason
+// Hello Paul
+// Hello Frank
+// Hello Larry
+// Hello Paula
+// // Hello Laura
+// Good Bye Jim
+for ( var name in names) {
+	if (names[name][0]=="J"|"j"){
+		console.log(byeSpeaker.speak(names[name]));
+	}
+	else{
+		console.log(helloSpeaker.speak(names[name]));
+	}
+}
